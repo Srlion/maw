e2cc82e851b74340a95d080036d0defcccab60cd
Srlion <srlion32@gmail.com>
2026-01-19 18:15:02 +0200

    refactor: simplify AsyncFn trait implementation by removing unnecessary bounds and unused attributes

--- src/async_fn.rs
+++ src/async_fn.rs
@@ -6,40 +6,29 @@ macro_rules! impl_async_fn {
     ($n:tt, [$($arg:ident),*]) => {
         paste::paste! {
             #[allow(non_snake_case)]
-            #[allow(unused)]
             pub trait [<AsyncFn $n>]<$($arg),*> {
-                type Output: Send;
+                type Output;
+                type Future: Future<Output = Self::Output> + Send;
 
-                fn call(&self, $($arg: $arg),*) -> impl Future<Output = Self::Output> + Send
-                where
-                    Self: Sync,;
+                fn call(&self, $($arg: $arg),*) -> Self::Future;
             }
 
             #[allow(non_snake_case)]
-            #[allow(unused)]
-            impl<F, Fut, $($arg),*> [<AsyncFn $n>]<$($arg),*> for F
+            impl<F: ?Sized, Fut, $($arg),*> [<AsyncFn $n>]<$($arg),*> for F
             where
-                F: Fn($($arg),*) -> Fut + Send + Sync,
+                F: Fn($($arg),*) -> Fut,
                 Fut: Future + Send,
-                Fut::Output: Send,
-                $($arg: Send,)*
             {
                 type Output = Fut::Output;
+                type Future = Fut;
 
-                fn call(&self, $($arg: $arg),*) -> impl Future<Output = Self::Output> + Send
-                where
-                    Self: Sync,
-                {
-                    async move { (self)($($arg),*).await }
+                fn call(&self, $($arg: $arg),*) -> Self::Future {
+                    (self)($($arg),*)
                 }
             }
         }
     };
 }
 
-impl_async_fn!(0, []);
 impl_async_fn!(1, [Arg0]);
 impl_async_fn!(2, [Arg0, Arg1]);
-impl_async_fn!(3, [Arg0, Arg1, Arg2]);
-impl_async_fn!(4, [Arg0, Arg1, Arg2, Arg3]);
-impl_async_fn!(5, [Arg0, Arg1, Arg2, Arg3, Arg4]);

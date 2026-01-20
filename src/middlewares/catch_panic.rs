f820a2dd0f5bcafde02bcfb4147d1cf1c9faaccf
Srlion <srlion32@gmail.com>
2026-01-20 21:14:59 +0200

    refactor: cleanup catch panic middleware

--- src/middlewares/catch_panic.rs
+++ src/middlewares/catch_panic.rs
@@ -6,61 +6,89 @@ use std::{
     task::{Context, Poll},
 };
 
-use crate::{async_fn::AsyncFn1, ctx::Ctx, prelude::StatusError};
+use http::StatusCode;
+use pin_project_lite::pin_project;
 
-pub struct CatchPanicMiddleware {
-    _p: (),
+use crate::{
+    async_fn::{AsyncFn1, AsyncFn2},
+    ctx::Ctx,
+};
+
+pub struct NoPanicHandler;
+
+pub struct CatchPanicMiddleware<F = NoPanicHandler> {
+    on_panic: F,
 }
 
-impl CatchPanicMiddleware {
+impl CatchPanicMiddleware<NoPanicHandler> {
     pub fn new() -> Self {
-        Self { _p: () }
+        Self {
+            on_panic: NoPanicHandler,
+        }
     }
 }
 
-impl Default for CatchPanicMiddleware {
-    fn default() -> Self {
-        Self::new()
+impl<F> CatchPanicMiddleware<F> {
+    pub fn on_panic<H>(self, f: H) -> CatchPanicMiddleware<H>
+    where
+        for<'a> H: AsyncFn2<&'a mut Ctx, Box<dyn Any + Send + 'static>, Output = ()> + Send + Sync,
+    {
+        CatchPanicMiddleware { on_panic: f }
     }
 }
 
-impl AsyncFn1<&mut Ctx> for CatchPanicMiddleware {
-    type Output = Result<(), StatusError>;
+impl AsyncFn1<&mut Ctx> for CatchPanicMiddleware<NoPanicHandler> {
+    type Output = ();
 
     async fn call(&self, c: &mut Ctx) -> Self::Output {
-        match AssertUnwindSafe(c.next()).catch_unwind().await {
-            Ok(_) => Ok(()),
-            Err(e) => {
-                let panic_msg = extract_panic_message(&e);
-                tracing::error!(error = %panic_msg, "panic occurred");
-                Err(StatusError::internal_server_error())
-            }
+        if let Err(e) = (CatchUnwind {
+            future: AssertUnwindSafe(c.next()),
+        })
+        .await
+        {
+            let panic_msg = extract_panic_message(&e);
+            tracing::error!(error = %panic_msg, "panic occurred");
+            c.res.status(StatusCode::INTERNAL_SERVER_ERROR);
+        }
+    }
+}
+
+impl<F> AsyncFn1<&mut Ctx> for CatchPanicMiddleware<F>
+where
+    for<'a> F: AsyncFn2<&'a mut Ctx, Box<dyn Any + Send + 'static>, Output = ()> + Send + Sync,
+{
+    type Output = ();
+
+    async fn call(&self, c: &mut Ctx) -> Self::Output {
+        if let Err(e) = (CatchUnwind {
+            future: AssertUnwindSafe(c.next()),
+        })
+        .await
+        {
+            self.on_panic.call(c, e).await;
         }
     }
 }
 
 fn extract_panic_message(e: &Box<dyn Any + Send>) -> String {
     e.downcast_ref::<&str>()
-        .map(|s| (*s).to_string())
+        .map(|s| s.to_string())
         .or_else(|| e.downcast_ref::<String>().cloned())
         .unwrap_or_else(|| format!("Unknown panic type: {e:#?}"))
 }
 
-trait FutureExt: Future + std::panic::UnwindSafe + Sized {
-    fn catch_unwind(self) -> CatchUnwind<Self> {
-        CatchUnwind(self)
+pin_project! {
+    pub struct CatchUnwind<F> {
+        #[pin]
+        future: F,
     }
 }
-
-impl<F: Future + std::panic::UnwindSafe> FutureExt for F {}
-
-struct CatchUnwind<F>(F);
 
 impl<F: Future + std::panic::UnwindSafe> Future for CatchUnwind<F> {
     type Output = Result<F::Output, Box<dyn Any + Send>>;
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        let f = unsafe { self.map_unchecked_mut(|s| &mut s.0) };
+        let f = self.project().future;
         catch_unwind(AssertUnwindSafe(|| f.poll(cx)))?.map(Ok)
     }
 }

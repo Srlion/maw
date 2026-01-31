d7c2915a31566a647ec0d4dcd94c059e9cc28e80
Srlion <srlion32@gmail.com>
2026-01-31 20:19:41 +0200

    chore: export serde_json

--- src/lib.rs
+++ src/lib.rs
@@ -76,4 +76,5 @@ pub mod prelude {
 pub use crate::into_response::IntoResponse;
 pub use crate::response::{BoxError, HttpBody};
 pub use app::config::Config;
+pub use serde_json;
 pub use tokio_util::sync::CancellationToken;

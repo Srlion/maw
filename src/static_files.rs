ebd61db61b8bfee2bf95db8bf4296d0f7e04072c
Srlion <srlion32@gmail.com>
2026-02-02 21:07:42 +0200

    feat: expose StaticFiles in the library and prelude modules

--- src/static_files.rs
+++ src/static_files.rs
@@ -1,71 +1,73 @@
-use std::path::PathBuf;
-
-use cap_std::fs::Dir;
 use http::StatusCode;
-use http_body_util::StreamBody;
-use tokio::fs::File;
-use tokio_util::io::ReaderStream;
+use rust_embed::RustEmbed;
+use std::marker::PhantomData;
 
 use crate::{async_fn::AsyncFn1, ctx::Ctx};
 
-pub struct StaticMiddleware {
-    dir: Dir,
-    stream_threshold: u64,
+pub struct StaticFiles<E> {
+    _marker: PhantomData<E>,
+    prefix: &'static str,
+    index: &'static str,
+    max_age: u32,
 }
 
-impl StaticMiddleware {
-    pub fn new(root: impl Into<PathBuf>) -> std::io::Result<Self> {
-        Ok(Self {
-            dir: Dir::open_ambient_dir(root.into(), cap_std::ambient_authority())?,
-            stream_threshold: 1_048_576,
-        })
+impl<E: RustEmbed> StaticFiles<E> {
+    pub fn new(prefix: &'static str) -> Self {
+        Self {
+            _marker: PhantomData,
+            prefix: prefix.trim_matches('/'),
+            index: "index.html",
+            max_age: 0,
+        }
     }
 
-    pub fn with_stream_threshold(mut self, bytes: u64) -> Self {
-        self.stream_threshold = bytes;
+    pub fn index(mut self, file: &'static str) -> Self {
+        self.index = file;
+        self
+    }
+
+    pub fn max_age(mut self, seconds: u32) -> Self {
+        self.max_age = seconds;
         self
     }
 }
 
-impl AsyncFn1<&mut Ctx> for StaticMiddleware {
+impl<E: RustEmbed + Send + Sync + 'static> AsyncFn1<&mut Ctx> for StaticFiles<E> {
     type Output = ();
 
     async fn call(&self, c: &mut Ctx) -> Self::Output {
-        let path = c.req.uri().path().trim_start_matches('/');
+        let uri_path = c.req.uri().path().trim_start_matches('/');
 
-        let metadata = match self.dir.metadata(path) {
-            Ok(m) if m.is_file() => m,
-            _ => {
-                c.next().await;
-                return;
-            }
+        // Strip prefix
+        let path = if self.prefix.is_empty() {
+            uri_path
+        } else if let Some(rest) = uri_path.strip_prefix(self.prefix) {
+            rest.trim_start_matches('/')
+        } else {
+            c.res.send_status(StatusCode::NOT_FOUND);
+            return;
+        };
+
+        // Try exact path, then with index
+        let file = if path.is_empty() || path.ends_with('/') {
+            E::get(&format!("{}{}", path, self.index))
+        } else {
+            E::get(path).or_else(|| E::get(&format!("{}/{}", path, self.index)))
+        };
+
+        let Some(file) = file else {
+            c.res.send_status(StatusCode::NOT_FOUND);
+            return;
         };
 
         let mime = mime_guess::from_path(path).first_or_octet_stream();
-
         c.res.set(("Content-Type", mime.as_ref()));
 
-        if metadata.len() > self.stream_threshold {
-            let file = match self.dir.open(path) {
-                Ok(f) => f,
-                Err(_) => {
-                    c.res.send_status(StatusCode::INTERNAL_SERVER_ERROR);
-                    return;
-                }
-            };
-
-            let stream = ReaderStream::new(File::from_std(file.into_std()));
-            let body = StreamBody::new(stream);
-            c.res.stream(body);
-        } else {
-            match self.dir.read(path) {
-                Ok(contents) => {
-                    c.res.send(contents);
-                }
-                Err(_) => {
-                    c.res.send_status(StatusCode::INTERNAL_SERVER_ERROR);
-                }
-            }
+        if self.max_age > 0 {
+            c.res
+                .set(("Cache-Control", format!("max-age={}", self.max_age)));
         }
+
+        c.res.send(file.data.into_owned());
     }
 }

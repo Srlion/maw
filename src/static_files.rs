808e1c11526195df96b068405a1ff7a8d9de2dc7
Srlion <srlion32@gmail.com>
2026-02-02 21:32:02 +0200

    cleanup some changes

--- src/static_files.rs
+++ src/static_files.rs
@@ -1,6 +1,11 @@
+use std::{
+    marker::PhantomData,
+    time::{Duration, UNIX_EPOCH},
+};
+
 use http::StatusCode;
+use httpdate::{fmt_http_date, parse_http_date};
 use rust_embed::RustEmbed;
-use std::marker::PhantomData;
 
 use crate::{async_fn::AsyncFn1, ctx::Ctx};
 
@@ -8,7 +13,7 @@ pub struct StaticFiles<E> {
     _marker: PhantomData<E>,
     prefix: &'static str,
     index: &'static str,
-    max_age: u32,
+    cache_control: Option<String>,
 }
 
 impl<E: RustEmbed> StaticFiles<E> {
@@ -17,7 +22,7 @@ impl<E: RustEmbed> StaticFiles<E> {
             _marker: PhantomData,
             prefix: prefix.trim_matches('/'),
             index: "index.html",
-            max_age: 0,
+            cache_control: None,
         }
     }
 
@@ -27,18 +32,21 @@ impl<E: RustEmbed> StaticFiles<E> {
     }
 
     pub fn max_age(mut self, seconds: u32) -> Self {
-        self.max_age = seconds;
+        self.cache_control = if seconds > 0 {
+            Some(format!("max-age={seconds}"))
+        } else {
+            None
+        };
         self
     }
 }
 
-impl<E: RustEmbed + Send + Sync + 'static> AsyncFn1<&mut Ctx> for StaticFiles<E> {
+impl<E: RustEmbed + Sync> AsyncFn1<&mut Ctx> for StaticFiles<E> {
     type Output = ();
 
     async fn call(&self, c: &mut Ctx) -> Self::Output {
         let uri_path = c.req.uri().path().trim_start_matches('/');
 
-        // Strip prefix
         let path = if self.prefix.is_empty() {
             uri_path
         } else if let Some(rest) = uri_path.strip_prefix(self.prefix) {
@@ -48,11 +56,10 @@ impl<E: RustEmbed + Send + Sync + 'static> AsyncFn1<&mut Ctx> for StaticFiles<E>
             return;
         };
 
-        // Try exact path, then with index
         let file = if path.is_empty() || path.ends_with('/') {
-            E::get(&format!("{}{}", path, self.index))
+            E::get(&[path, self.index].concat())
         } else {
-            E::get(path).or_else(|| E::get(&format!("{}/{}", path, self.index)))
+            E::get(path).or_else(|| E::get(&[path, self.index].concat()))
         };
 
         let Some(file) = file else {
@@ -60,12 +67,27 @@ impl<E: RustEmbed + Send + Sync + 'static> AsyncFn1<&mut Ctx> for StaticFiles<E>
             return;
         };
 
+        if let Some(last_modified) = file.metadata.last_modified() {
+            let modified = UNIX_EPOCH + Duration::from_secs(last_modified);
+            c.res.set(("Last-Modified", fmt_http_date(modified)));
+
+            if let Some(ims) = c.req.headers().get("If-Modified-Since") {
+                if let Ok(ims_str) = ims.to_str() {
+                    if let Ok(ims_time) = parse_http_date(ims_str) {
+                        if modified <= ims_time {
+                            c.res.send_status(StatusCode::NOT_MODIFIED);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
         let mime = mime_guess::from_path(path).first_or_octet_stream();
         c.res.set(("Content-Type", mime.as_ref()));
 
-        if self.max_age > 0 {
-            c.res
-                .set(("Cache-Control", format!("max-age={}", self.max_age)));
+        if let Some(cc) = &self.cache_control {
+            c.res.set(("Cache-Control", cc.as_str()));
         }
 
         c.res.send(file.data.into_owned());
